23227d03acc9147ba610385a3da348f495174ff5
vatsalrami96 <vatsalrami96@gmail.com>
2025-09-24 02:15:59 -0700

    Create 3056-DetermineIfACellIsReachableAtAGivenTime

---
+++ 3056-DetermineIfACellIsReachableAtAGivenTime/3056-DetermineIfACellIsReachableAtAGivenTime.cpp
@@ -0,0 +1,11 @@
+// Last updated: 9/24/2025, 2:15:58 AM
+class Solution {
+public:
+    bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        int xdiff =abs(sx-fx);
+        int ydiff =abs(sy-fy);
+        if(xdiff==0 && ydiff ==0 && t==1) return false;
+        return max(xdiff,ydiff) <=t;
+
+    }
+};
